33488e77cfa043c535141f1e34937b26067df326
huo yongfei <773459418@qq.com>
2019-04-06 11:27:53 +0800

    finished function_extend

--- src/function_extend.js
+++ src/function_extend.js
@@ -32,3 +32,34 @@
 // 上面代码运行时，点击按钮会报错，因为button的监听函数是一个箭头函数，导致里面的this就是全局对象。如果改成普通函数，this就会动态指向被点击的按钮对象。
 
 // 另外，如果函数体很复杂，有许多行，或者函数内部有大量的读写操作，不单纯是为了计算值，这时也不应该使用箭头函数，而是要使用普通函数，这样可以提高代码可读性。
+
+//尾调用由于是函数的最后一步操作，所以不需要保留外层函数的调用帧，因为调用位置、内部变量等信息都不会再用到了，只要直接用内层函数的调用帧，取代外层函数的调用帧就可以了。
+
+//递归非常耗费内存，因为需要同时保存成千上百个调用帧，很容易发生“栈溢出”错误（stack overflow）。但对于尾递归来说，由于只存在一个调用帧，所以永远不会发生“栈溢出”错误。
+
+//不使用尾递归， 计算n的阶乘， 最多需要保存 n 个调用记录， 复杂度 O(n)
+const factorial = n => {
+  if (n === 1) return 1
+  return n * factorial(n - 1)
+}
+
+//改写成尾递归， 只保留一个调用记录， 复杂度O(1)
+
+const factorial_optimize = (n, total = 1) => {
+  if (n === 1) return total
+  return factorial_optimize(n - 1, n * total)
+}
+
+const Fibonacci = n => {
+    if(n <= 1) return 1
+
+    return Fibonacci(n - 1) + Fibonacci(n - 2)
+}
+
+const Fibonacci_optimize = (n, ac1 = 1, ac2 = 1) {
+    if(n <= 1) return ac2
+
+    return Fibonacci_optimize(n - 1, ac2, ac1 + ac2)
+}
+
+//ES2017 允许函数的最后一个参数有尾逗号（trailing comma）。
--- src/string_extend.js
+++ src/string_extend.js
@@ -1,14 +1,14 @@
 //字符的 Unicode 表示
 const a = '\u0061'
-console.log(a === 'a')
+// console.log(a === 'a')
 
 //js 共有6中方法可以表示一个字符串
 
-console.log('z' === 'z')
+// console.log('z' === 'z')
 // console.log('\172' === 'z')
-console.log('\x7A' === 'z')
-console.log('\u007A' === 'z')
-console.log('\u{7A}' === 'z')
+// console.log('\x7A' === 'z')
+// console.log('\u007A' === 'z')
+// console.log('\u{7A}' === 'z')
 
 //string.codePointAt
 //JavaScript 内部，字符以 UTF-16 的格式储存，每个字符固定为2个字节。对于那些需要4个字节储存的字符（Unicode 码点大于0xFFFF的字符），JavaScript 会认为它们是两个字符。
